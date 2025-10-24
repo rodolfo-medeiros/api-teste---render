3dff1f2830dc9029b1d8278a1f8da2eb03e66474
Rodolfo Medeiros <rodolfo.medeiros222@gmail.com>
2025-10-24 20:26:25 -0300

    atualizacao nome variavel vendas_produto

--- request.py
+++ request.py
@@ -2,5 +2,7 @@ import requests
 import json
 
 
-dict_vendas_produtos = requests.get("https://api-teste-render-xgxa.onrender.com/vendas/produtos/<Terno Xadrez>").json()
+dict_vendas_produtos = requests.get("https://api-teste-render-xgxa.onrender.com/vendas/produtos").json()
+
 print(dict_vendas_produtos)
+
