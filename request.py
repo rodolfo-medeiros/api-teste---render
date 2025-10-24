dd5993a94ffc7f9bc425453a8a054d8c3ceee8d4
Rodolfo Medeiros <150673607+rodolfo-medeiros@users.noreply.github.com>
2025-10-24 20:29:37 -0300

    Update API endpoint for fetching product sales

--- request.py
+++ request.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-dict_vendas_produtos = requests.get("https://api-teste-render-xgxa.onrender.com/vendas/produtos").json()
+dict_vendas_produtos = requests.get("https://api-teste-render-xgxa.onrender.com/vendas/produtos/<Polo Estampa").json()
 
 print(dict_vendas_produtos)
 
