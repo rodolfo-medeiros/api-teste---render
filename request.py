620210ccca05e3244413fb355365a0ac6d4d00ea
Rodolfo Medeiros <rodolfo.medeiros222@gmail.com>
2025-10-24 20:12:27 -0300

    update def vendas_produto_especificio

--- request.py
+++ request.py
@@ -2,5 +2,5 @@ import requests
 import json
 
 
-dict_vendas_produtos = requests.get("https://api-teste-render-xgxa.onrender.com/vendas/produtos").json()
+dict_vendas_produtos = requests.get("https://api-teste-render-xgxa.onrender.com/vendas/produtos/<Terno Xadrez>").json()
 print(dict_vendas_produtos)
